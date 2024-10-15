abec51b617dfaee960dd879c3cf4386f4b225172
Konrad Rollenbeck <konrad.rollenbeck@gmail.com>
2024-10-15 17:14:59 +0200

    comments

--- iPhone.py
+++ iPhone.py
@@ -1,6 +1,6 @@
 import os
 
-file = open("/private/var/db/os_eligibility/eligibility.plist")
+file = open("/private/var/db/os_eligibility/eligibility.plist") #file that contains a few numbers that are important for iPhone Mirroring
 
 newFile = ""
 
@@ -8,7 +8,7 @@ i = 0
 for line in file:
     if "IRON" in line:
         newFile += line
-        i = 179
+        i = 179 #line 179 is the line where the important part of the file starts for me. YOU DON'T NEED TO CHANGE ANYTHING. It counts relative to the "IRON" line
     elif i!=0:
         i += 1
         match i:
